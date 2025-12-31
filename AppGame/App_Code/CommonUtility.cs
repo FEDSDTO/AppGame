502c8a89d2cc4dd11df407c9be3e0b9c1bc5b1c5
HQFEG\feds02424 <pojhihchen@feds.com.tw>
2025-12-31 17:13:33 +0800

    修正正式環境路徑

--- AppGame/App_Code/CommonUtility.cs
+++ AppGame/App_Code/CommonUtility.cs
@@ -12,7 +12,7 @@ namespace AppGame.App_Code
     {
         public void Txt(string ex)
         {
-            string sourcePath = @"C:\Website\APPGame\Log" + @"\" + DateTime.Now.ToString("yyyy-MM-dd") + "狀態.txt";       //正式
+            string sourcePath = @"D:\Website\APPGame\Log" + @"\" + DateTime.Now.ToString("yyyy-MM-dd") + "狀態.txt";       //正式
             //string sourcePath = @"C:\Website\APPGame\Log\" + DateTime.Now.ToString("yyyy-MM-dd") + "狀態.txt";     //測試
             //string sourcePath = @"E:\POJHIH\Website\APPGame\Log\" + DateTime.Now.ToString("yyyy-MM-dd") + "狀態.txt";    //本地
             #region  寫入txt檔
