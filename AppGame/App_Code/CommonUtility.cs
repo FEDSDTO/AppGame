af7000e6363c04c3b8d127dbe11cf936c44ddb42
HQFEG\feds02424 <pojhihchen@feds.com.tw>
2025-12-31 13:58:34 +0800

    更改環境為測試環境

--- AppGame/App_Code/CommonUtility.cs
+++ AppGame/App_Code/CommonUtility.cs
@@ -13,8 +13,8 @@ namespace AppGame.App_Code
         public void Txt(string ex)
         {
             //string sourcePath = @"C:\Website\APPGame\Log" + @"\" + DateTime.Now.ToString("yyyy-MM-dd") + "狀態.txt";       //正式
-            //string sourcePath = @"C:\Website\APPGame\Log\" + DateTime.Now.ToString("yyyy-MM-dd") + "狀態.txt";     //測試
-            string sourcePath = @"E:\POJHIH\Website\APPGame\Log\" + DateTime.Now.ToString("yyyy-MM-dd") + "狀態.txt";    //本地
+            string sourcePath = @"C:\Website\APPGame\Log\" + DateTime.Now.ToString("yyyy-MM-dd") + "狀態.txt";     //測試
+            //string sourcePath = @"E:\POJHIH\Website\APPGame\Log\" + DateTime.Now.ToString("yyyy-MM-dd") + "狀態.txt";    //本地
             #region  寫入txt檔
             string txt = "";
             if (File.Exists(sourcePath))
